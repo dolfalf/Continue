f7f15130f86eb8e4b28da06213f536fa8431814f
lee jaeeun <leejaeeun@sinfo-inc.co.jp>
2016-10-25 13:43:36 +0900

    first commit!

---
+++ Continue/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Continue
+//
+//  Created by lee jaeeun on 2016/10/25.
+//  Copyright © 2016年 lee jaeeun. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
